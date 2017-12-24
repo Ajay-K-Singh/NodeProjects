a96c055bfe330ee6b784269525b1e66ae6c25b07
Ajay Singh <aksingh1493@gmail.com>
2017-12-24 18:59:43 +0530

    Add note function added

--- NoteApp/notes.js
+++ NoteApp/notes.js
@@ -1,5 +1,22 @@
+const fs = require('fs');
 var addNote = (title, body) => {
-    console.log('Adding Note', title, body);
+    var notes = [];
+    const note = {
+        title,
+        body
+    };
+    try {
+        const noteString = fs.readFileSync('notes.json');
+        notes = JSON.parse(noteString);
+    } catch (e) {
+        console.log(e);
+    }
+    var duplicateNotes = notes.filter((note) => note.title === title);
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        fs.writeFileSync('notes.json', JSON.stringify(notes));
+    }
+
 };
 
 var getAll = () => {
